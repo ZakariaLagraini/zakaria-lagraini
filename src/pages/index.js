0e95dd76fb6eee50a1f9e32cbe34d2fb5dd1c299
Zakaria Lagraini <zakarialaganimes@gmail.com>
2024-06-20 13:32:41 +0100

    add the toggle button of dark and light mode

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,18 +7,36 @@ import Hero from '../components/Hero/Hero';
 import Projects from '../components/Projects/Projects';
 import Technologies from '../components/Technologies/Technologies';
 import Timeline from '../components/TimeLine/TimeLine';
-import { FiMessageSquare } from 'react-icons/fi'; // Importing the chat icon
+import { FiMessageSquare } from 'react-icons/fi';
+import { FiSun, FiMoon } from 'react-icons/fi';
+import { useDarkMode } from 'next-dark-mode';
 import styles from '../styles/Chatbot.module.css';
 
 const Home = () => {
   const [isChatbotVisible, setIsChatbotVisible] = useState(false);
+  const { darkModeActive, switchToDarkMode, switchToLightMode } = useDarkMode();
 
   const toggleChatbot = () => {
     setIsChatbotVisible(!isChatbotVisible);
   };
 
+  const toggleTheme = () => {
+    if (darkModeActive) {
+      switchToLightMode();
+    } else {
+      switchToDarkMode();
+    }
+  };
+
   return (
     <Layout>
+      <div className={styles.themeToggleButton} onClick={toggleTheme}>
+        {darkModeActive ? (
+          <FiSun size={24} color="yellow" />
+        ) : (
+          <FiMoon size={24} color="black" />
+        )}
+      </div>
       <Section grid center>
         <Hero />
         <BgAnimation />
